335801786e8d64e941f8d904773c8f4f95ff2087
Your Name <you@example.com>
2025-10-18 18:47:24 +0300

    Register end point /api/auth/register

--- VitaLink/Dtos/RegisterDto.cs
+++ VitaLink/Dtos/RegisterDto.cs
@@ -1,6 +1,32 @@
-﻿namespace VitaLink.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vitalink.API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; } // سيُستخدم كـ Username
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } // كلمة المرور الأصلية (Plain Text)
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public double Weight { get; set; } // الوزن (كجم)
+
+        public double BodyFatPercentage { get; set; } // نسبة الدهون (%)
+
+        [StringLength(5)]
+        public string? BloodType { get; set; } // فصيلة الدم
+
+        [StringLength(50)]
+        public string? TargetSport { get; set; } // الرياضة المستهدفة
     }
 }

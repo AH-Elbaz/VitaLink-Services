cbdd24ce45e3e5cd4875f42e5e5f7e28d4eae4c4
AH-Elbaz <AHMED12300291@cs.modern-academy.edu.eg>
2025-11-12 18:33:55 +0200

    Update SensorDataHub.cs

--- VitaLink/Hubs/SensorDataHub.cs
+++ VitaLink/Hubs/SensorDataHub.cs
@@ -6,27 +6,24 @@
     using Vitalink.API.Dtos;
     using Vitalink.API.Services;
     using VitaLink.Models.Data;
-    using Vitalink.API.Controllers;
 
     namespace Vitalink.API.Hubs
     {
         public class SensorDataHub : Hub
         {
             private readonly ConnectionTracker _tracker;
-            private readonly VitalinkDbContext _dbContext;
-        private readonly AthleteProfilesController _athleteProfilesController;
+            private readonly VitalinkDbContext _dbContext; 
 
+            
+            public SensorDataHub(ConnectionTracker tracker, VitalinkDbContext dbContext)
+            {
+                _tracker = tracker;
+                _dbContext = dbContext;
+            }
 
-        public SensorDataHub(ConnectionTracker tracker, VitalinkDbContext dbContext, AthleteProfilesController athleteProfilesController)
-        {
-            _tracker = tracker;
-            _dbContext = dbContext;
-            _athleteProfilesController = athleteProfilesController;
-        }
+         
 
-
-
-        public async Task RegisterConnection(string username)
+            public async Task RegisterConnection(string username)
             {
                 
                 _tracker.AddConnection(username, Context.ConnectionId);
@@ -46,18 +43,14 @@
                                               .Select(a => a.FirstName) 
                                               .FirstOrDefaultAsync();
 
-
-            if (targetUsername != null)
+                if (targetUsername != null)
                 {
              
                 var targetConnectionIds = _tracker.GetConnectionIds(targetUsername);
 
                 if (targetConnectionIds.Any())
                     {
-
-                    // await _athleteProfilesController.RawData(data);
-
-              
+                   
                     await Clients.Clients(targetConnectionIds.ToList()).SendAsync("ReceiveLiveUpdate", data);
 
                     Debug.WriteLine($"[STREAM SUCCESS] Data routed to {targetConnectionIds.Count()} connection(s) for user {targetUsername}.");
@@ -84,4 +77,3 @@
             }
         }
     }
-

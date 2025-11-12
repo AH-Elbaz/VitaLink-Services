306646c45c4ca5dc819541032efd10b48fcf8c61
AH-Elbaz <AHMED12300291@cs.modern-academy.edu.eg>
2025-11-12 18:59:09 +0200

    Update SensorDataRaw.cs

--- VitaLink/Models/SensorDataRaw.cs
+++ VitaLink/Models/SensorDataRaw.cs
@@ -1,4 +1,3 @@
-﻿// ملف: Models/SensorDataRaw.cs
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,26 +5,23 @@ namespace Vitalink.Models
 {
     public class SensorDataRaw
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }  // unique key for each reading
 
         [Required]
         public string BeltID { get; set; }
 
-
         [Required]
         public float HeartRate { get; set; }
 
-
         public byte Spo2 { get; set; }
-
-
         public float Temperature { get; set; }
-
-
         public float AccX { get; set; }
         public float AccY { get; set; }
         public float AccZ { get; set; }
-
-
         public ushort Sweat { get; set; }
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     }
 }
